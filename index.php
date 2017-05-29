58fd00be2dbb5c1b0094b79852b527217a8a91e3
Marcus Soares <marcus@flexpeak.com.br>
2017-05-29 13:05:53 -0300

    tirando comentarios sobre pessoa

--- index.php
+++ index.php
@@ -6,23 +6,23 @@ $loader = require 'vendor/autoload.php';
 $app = new Slim(array(
     'templates.path' => 'templates'
 ));
-//listando todos
+
 $app->get("/fornecedor/", function() use ($app){
     (new Fornecedor($app))->lista();
 });
-//listando um
+
 $app->get("/fornecedor/:id", function($id) use ($app){
     (new Fornecedor($app))->get($id);
 });
-//cadastro - nova pessoa
+
 $app->post("/fornecedor/", function() use ($app){
     (new Fornecedor($app))->nova();
 });
-//editando pessoa
+
 $app->put("/fornecedor/:id", function($id) use ($app){
     (new Fornecedor($app))->editar($id);
 });
-//apagando pessoa
+
 $app->delete("/fornecedor/:id", function($id) use ($app){
     (new Fornecedor($app))->excluir($id);
 });
